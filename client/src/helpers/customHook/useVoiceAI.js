eb41946715554127aaf5c828c2a96dd0b2675a14
Abdullahi Hussein <miraacle64@gmail.com>
2022-03-31 20:25:56 +0100

    feat:implemented the ai-assistant to change theme,still buggy though

--- client/src/helpers/customHook/useVoiceAI.js
+++ client/src/helpers/customHook/useVoiceAI.js
@@ -1,33 +1,41 @@
 import React,{useEffect,useRef,useState} from "react";
 import alanBtn  from "@alan-ai/alan-sdk-web";
+import { useDispatch, useSelector } from "react-redux";
+import {changeTheme} from "../../store/slices/themeSlice";
 
 const useVoiceAI = () => {
-    const [alanBtnInstance ,setAlanBtnInstance] = useState()
-    //AI-ASSISTANCE
-//   const alanBtnInstance = useRef(null);
+    const [themeCheck,setThemeCheck] = useState("")
+    const  dispatch = useDispatch();
+    const theme = useSelector((state)=>state.themeApi.theme);
+    const currentTheme = theme =="dark"? 'navy-blue':'dark'
+    let alanBtnInstance = useRef(null);
 
     useEffect(() => {
-        if(alanBtnInstance !== null) return ;
-        // if (!alanBtnInstance.current) {
-        //     alanBtnInstance.current = 
-        // }
+        if (!alanBtnInstance.current) {
+            alanBtnInstance.current = alanBtn({
+                top:"20px",
+                left:"20px",
+                rootEl: document.getElementById("alan-btn"),
 
-        setAlanBtnInstance(
-            alanBtn({
-                top:"15px",
-                left:"15px",
-                key: process.env.ALAN_KEY,
-                    onCommand: (commandData) => {
-                        console.log(commandData)
-                        if (commandData.command === 'go:back') {
-                        // Call the client code that will react to the received command
-                        }
-                    }
-            }) 
-            )
-    }, []);
+            key: process.env.REACT_APP_ALAN_AI_KEY,
+
+            onCommand: ({command}) => {
+                console.log(command,"[command]")
+                if(command == "change-theme"){
+                    setThemeCheck(currentTheme);
+                    dispatch(changeTheme({ theme:currentTheme }));
+                }
+
+                
+            }
+            });
+        }
+
+    }, [currentTheme,theme,themeCheck]);
 
 
-    return null;
+    return (
+        <div className="alan-btn" id="alan-btn" ref={alanBtnInstance}>   </div>
+    )
 }
 export default useVoiceAI;
